dfa82844d680c201ede9cf19ebde2e4923879298
Hunter Jennings <jenningsdhunter@gmail.com>
2023-07-15 18:00:33 -0400

    wip: installing base project deps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  typescript: {
+    // We can safely ignore build errors when using TypeScript because we're type-checking the code in CI
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
