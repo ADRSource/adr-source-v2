def7eaf535fb9f43b7ecd622364ccc06fed63cd9
Hunter Jennings <jenningsdhunter@gmail.com>
2023-07-15 18:15:22 -0400

    add: CI

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // We can safely ignore ESLint errors linting the code in CI
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // We can safely ignore build errors when using TypeScript because we're type-checking the code in CI
     ignoreBuildErrors: true,
