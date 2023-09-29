7af1c4684dc479dfa3bc4f10a08489f85b276a03
Hunter Jennings <jenningsdhunter@gmail.com>
2023-09-29 16:27:13 -0400

    hotfix: stacking context

--- src/app/components/blocks/block-resources.tsx
+++ src/app/components/blocks/block-resources.tsx
@@ -6,25 +6,27 @@ import { heading, text } from '~/components/ui/text';
 export function BlockResources() {
 	return (
 		<Container>
-			<div className="relative py-7 stack-y-3">
-				<h2
-					className={heading({
-						type: '3',
-						className: ' text-left md:text-right',
-					})}
-				>
-					Resources
-				</h2>
-				<AutoGrid count={4} itemMinWidth={350} gapX="24px" gapY="24px" className="relative z-20">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-				</AutoGrid>
-				<div className="flex justify-center">
-					<ButtonLink href="/resources" size="small">
-						View All
-					</ButtonLink>
+			<div className="relative py-7">
+				<div className="relative z-20 stack-y-3">
+					<h2
+						className={heading({
+							type: '3',
+							className: ' text-left md:text-right',
+						})}
+					>
+						Resources
+					</h2>
+					<AutoGrid count={4} itemMinWidth={350} gapX="24px" gapY="24px" className="relative z-20">
+						<Card />
+						<Card />
+						<Card />
+						<Card />
+					</AutoGrid>
+					<div className="flex justify-center">
+						<ButtonLink href="/resources" size="small">
+							View All
+						</ButtonLink>
+					</div>
 				</div>
 				<div className="pointer-events-none absolute bottom-0 z-10 aspect-square w-[min(75%,_787px)] rounded-full bg-brand-copper/30 bg-blend-darken blur-[250px] inset-x-center md:bottom-[-20%]" />
 			</div>
