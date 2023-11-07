dfe5d17a1183bd6e4f55383361348be8b62ad4fd
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-07 11:28:38 -0500

    fix: init client in a function

--- src/graphql/cms.ts
+++ src/graphql/cms.ts
@@ -1,6 +1,7 @@
+import 'server-only';
+
 import { GraphQLClient } from 'graphql-request';
 import { draftMode } from 'next/headers';
-import 'server-only';
 import { CMS_URL } from '~/constants/cms.constants';
 import { getSdk } from '~/graphql/generated/cms.generated';
 
@@ -10,12 +11,15 @@ function getAuthHeader() {
 	return `Bearer ${isEnabled ? process.env.CMS_PREVIEW_TOKEN : process.env.CMS_PROD_TOKEN}`;
 }
 
-const client = new GraphQLClient(CMS_URL, {
-	headers: {
-		Authorization: getAuthHeader(),
-	},
-});
+function getClient() {
+	return new GraphQLClient(CMS_URL, {
+		headers: {
+			Authorization: getAuthHeader(),
+		},
+		fetch,
+	});
+}
 
 export function cmsRequest() {
-	return getSdk(client);
+	return getSdk(getClient());
 }
