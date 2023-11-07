8ca9ea65d493814907e2915db315792245f27939
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-07 12:04:31 -0500

    Revert "fix: init client in a function"
    This reverts commit dfe5d17a1183bd6e4f55383361348be8b62ad4fd.

--- src/graphql/cms.ts
+++ src/graphql/cms.ts
@@ -1,7 +1,6 @@
-import 'server-only';
-
 import { GraphQLClient } from 'graphql-request';
 import { draftMode } from 'next/headers';
+import 'server-only';
 import { CMS_URL } from '~/constants/cms.constants';
 import { getSdk } from '~/graphql/generated/cms.generated';
 
@@ -11,15 +10,12 @@ function getAuthHeader() {
 	return `Bearer ${isEnabled ? process.env.CMS_PREVIEW_TOKEN : process.env.CMS_PROD_TOKEN}`;
 }
 
-function getClient() {
-	return new GraphQLClient(CMS_URL, {
-		headers: {
-			Authorization: getAuthHeader(),
-		},
-		fetch,
-	});
-}
+const client = new GraphQLClient(CMS_URL, {
+	headers: {
+		Authorization: getAuthHeader(),
+	},
+});
 
 export function cmsRequest() {
-	return getSdk(getClient());
+	return getSdk(client);
 }
