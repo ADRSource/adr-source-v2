df05043fd3d9542263c19530b7ac6b3e0c212128
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-09 16:30:18 -0500

    add: wire up profile page content to CMS

--- src/graphql/queries/member.queries.ts
+++ src/graphql/queries/member.queries.ts
@@ -1,5 +1,5 @@
-import gql from "graphql-tag";
-
+import gql from 'graphql-tag';
+import { SeoFragment } from '~/graphql/fragments/seo.fragments';
 
 export const GetNeutralList = gql`
 	query GetNeutralList {
@@ -37,4 +37,74 @@ export const GetCaseManagerList = gql`
 			}
 		}
 	}
+`;
+
+export const GetMemberPageBySlug = gql`
+	query GetMemberPageBySlug($slug: String!) {
+		memberPage(where: { slug: $slug }) {
+			seo {
+				...SeoInfo
+			}
+			slug
+			member {
+				__typename
+				...MemberInfoNeutral
+				...MemberInfoCaseManager
+			}
+		}
+	}
+
+	fragment MemberInfoNeutral on Neutral {
+		__typename
+		id
+		info {
+			...BaseMemberInfo
+		}
+		experienceStartDate
+		focusAreas
+		nadnId
+		caseManager {
+			... on CaseManager {
+				id
+				memberPage {
+					slug
+				}
+				info {
+					name
+				}
+			}
+		}
+	}
+
+	fragment MemberInfoCaseManager on CaseManager {
+		__typename
+		id
+		info {
+			...BaseMemberInfo
+		}
+		neutrals {
+			id
+			memberPage {
+				slug
+			}
+			info {
+				name
+			}
+		}
+	}
+
+	fragment BaseMemberInfo on BaseMemberInfo {
+		name
+		headshot {
+			url(transformation: { document: { output: { format: webp } } })
+		}
+		email
+		phone
+		linkedIn
+		bio {
+			raw
+		}
+	}
+
+	${SeoFragment}
 `;
