8102beb90434e0fd7f594dda94c5fee1c144505c
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-13 21:39:04 -0500

    add: analytics package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { DraftModeControls } from '~/components/draft-mode-controls';
 import { Footer } from '~/components/footer/footer';
@@ -41,6 +42,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				{children}
 				<Footer />
 				<DraftModeControls />
+				<Analytics />
 			</body>
 		</html>
 	);
