1f3bbecd06bcc09ae8bc270c2f1741eb08cac48a
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-15 08:45:21 -0500

    fix: attempt to fix the blurry text on windows

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -16,8 +16,9 @@ export function Navigation() {
 function DesktopNavigation() {
 	return (
 		<header className="fixed top-2 z-30 hidden h-nav-height w-full px-2 inset-x-center md:block">
-			<nav className="mx-auto flex h-full w-full max-w-[922px] items-center rounded-full border border-solid border-brand-copper bg-brand-black/70 backdrop-blur-sm backdrop-saturate-150">
-				<ul className="relative flex w-full items-center justify-between px-2 py-1 pl-[calc(theme(spacing.1)_*_2)] stack-x-2">
+			<nav className="relative mx-auto grid h-full w-full max-w-[922px] place-items-center">
+				<div className="absolute z-10 h-full w-full rounded-full border border-solid border-brand-copper bg-brand-black/70 backdrop-blur-sm backdrop-saturate-150" />
+				<ul className="relative z-20 flex w-full items-center justify-between px-2 py-1 pl-[calc(theme(spacing.1)_*_2)] stack-x-2">
 					<li>
 						<Link href={PATHS.home} className="absolute left-2 inset-y-center">
 							<IconLogo className="h-[25px] w-auto md:h-[30px]" />
