5c0b8c56c3873226925e0406ea367ae4fc9b29a2
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-16 21:48:25 -0500

    refactor: to be proper marquee

--- src/components/footer/call-to-action.tsx
+++ src/components/footer/call-to-action.tsx
@@ -5,7 +5,7 @@ import { Marquee } from './marquee';
 export function CallToAction() {
 	return (
 		<div className="grid grid-cols-1 grid-rows-1 place-items-center border-y border-brand-black">
-			<Marquee>When it counts</Marquee>
+			<Marquee text="When it counts. " />
 			<div className="relative z-20 col-span-full row-span-full">
 				<ButtonLink href={PATHS.schedule} size="large" outline="black">
 					Schedule
--- src/components/footer/marquee.tsx
+++ src/components/footer/marquee.tsx
@@ -1,83 +1,41 @@
 'use client';
-import * as React from 'react';
 
-import {
-	MotionValue,
-	motion,
-	useMotionTemplate,
-	useMotionValueEvent,
-	useScroll,
-	useSpring,
-} from 'framer-motion';
-import { mergeRefs } from 'react-merge-refs';
+import { motion } from 'framer-motion';
 import useMeasure from 'react-use-measure';
-import { useIsClient } from '~/hooks/use-is-client';
 import { useMediaQuery } from '~/hooks/use-media-query';
 
-export function Marquee({ children }: { children: React.ReactNode }) {
-	const containerRef = React.useRef<HTMLDivElement>(null);
-	const [containerMeasureRef, { width: containerWidth }] = useMeasure({ debounce: 100 });
-	const [textMeasureRef, { width: textWidth }] = useMeasure({ debounce: 100 });
-	const { scrollYProgress } = useScroll({
-		target: containerRef,
-		offset: ['end end', '200% end'],
-	});
-	const textWidthDelta = textWidth - containerWidth;
+const REPEAT_COUNT = 10;
 
-	const springScrollYProgress = useSpring(scrollYProgress, {
-		stiffness: 100,
-		damping: 30,
-		restDelta: 0.001,
-	});
-	const translateX = useMotionTemplate`calc(100% - (${
-		textWidth + textWidthDelta * springScrollYProgress.get()
-	}px))`;
-	useMotionValueEvent(springScrollYProgress, 'change', (v) => {
-		translateX.set(`calc(100% - (${textWidth + textWidthDelta * v}px))`);
-	});
+export function Marquee({ text }: { text: string }) {
+	const textRepeat = text.repeat(REPEAT_COUNT);
+	const [textMeasureRef, { width }] = useMeasure({ debounce: 100 });
+	const isMobile = useMediaQuery('(max-width: 640px)');
 
 	return (
-		<div
-			ref={mergeRefs([containerRef, containerMeasureRef])}
-			className="relative z-10 col-span-full row-span-full h-full w-full select-none overflow-hidden"
-		>
+		<div className="relative z-10 col-span-full row-span-full h-full w-full select-none overflow-hidden">
 			<p
 				aria-hidden
 				className="leading-0 invisible whitespace-nowrap text-center font-serif text-[max(128px,14.5vw)] leading-[85%]"
 			>
-				{children}
+				{textRepeat}
 			</p>
-			<MarqueeText ref={textMeasureRef} translateX={translateX}>
-				{children}
-			</MarqueeText>
+			<motion.p
+				className="leading-0 absolute left-0 top-0 whitespace-nowrap text-center font-serif text-[max(128px,14.5vw)] leading-[85%]"
+				ref={textMeasureRef}
+				animate={{
+					x: [0, -width],
+					transition: {
+						x: {
+							duration: isMobile ? 120 : 300,
+							repeat: Infinity,
+							repeatType: 'loop',
+							ease: 'linear',
+						},
+					},
+				}}
+			>
+				{textRepeat}
+			</motion.p>
 		</div>
 	);
 }
-
-interface MarqueeTextProps {
-	translateX: MotionValue<string>;
-	children: React.ReactNode;
-}
-const MarqueeText = React.forwardRef<HTMLParagraphElement, MarqueeTextProps>(
-	({ translateX, children }, ref) => {
-		const isMobile = useMediaQuery('(max-width: 880px)');
-		const isClient = useIsClient();
-
-		return isClient ? (
-			<motion.p
-				style={
-					{
-						x: isMobile ? translateX : '-50%',
-						left: isMobile ? '0' : '50%',
-					} as React.CSSProperties
-				}
-				className="leading-0 absolute left-0 top-0 whitespace-nowrap text-center font-serif text-[max(128px,14.5vw)] leading-[85%]"
-				ref={ref}
-			>
-				{children}
-			</motion.p>
-		) : null;
-	},
-);
-
-MarqueeText.displayName = 'MarqueeText';
