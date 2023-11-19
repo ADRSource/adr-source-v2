d475cc24150288b879693d8fe4755d717e7b18e9
Hunter Jennings <jenningsdhunter@gmail.com>
2023-11-19 16:28:53 -0500

    remove: page loader
    This was causing some strange issues with the hash links. It's frankly not worth dealing with.

--- src/app/(with-color-blur)/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { PageLoader } from '~/components/page-loader/page-loader';
-
-export default function Loading() {
-	return (
-		<div className="grid h-full min-h-[calc(100vh_-_theme(spacing.nav-height)_-_theme(spacing.1))] place-items-center md:min-h-[calc(100vh_-_theme(spacing.nav-height)_-_theme(spacing.2))]">
-			<PageLoader />
-		</div>
-	);
-}
