6b415c181b9ec60c61dc8a11b66ce808789daa0f
Hunter Jennings <jenningsdhunter@gmail.com>
2023-12-28 18:32:10 -0500

    fix: flicker on safari by adding will-change

--- src/components/blocks/block-hero/block-hero.tsx
+++ src/components/blocks/block-hero/block-hero.tsx
@@ -53,15 +53,15 @@ export function BlockHero() {
 			>
 				<div
 					aria-hidden
-					className="pointer-events-none absolute bottom-0 left-0 z-10 aspect-square w-[50%] -translate-x-1/2 translate-y-[33%] rounded-full bg-brand-blue opacity-10 blur-[70px]"
+					className="pointer-events-none absolute bottom-0 left-0 z-10 aspect-square w-[50%] -translate-x-1/2 translate-y-[33%] rounded-full bg-brand-blue opacity-10 blur-[70px] will-change-transform"
 				/>
 				<div
 					aria-hidden
-					className="pointer-events-none absolute right-0 top-0 z-10 aspect-square w-[50%] -translate-y-[33%] translate-x-1/2 rounded-full bg-brand-red opacity-40 blur-[70px]"
+					className="pointer-events-none absolute right-0 top-0 z-10 aspect-square w-[50%] -translate-y-[33%] translate-x-1/2 rounded-full bg-brand-red opacity-40 blur-[70px] will-change-transform"
 				/>
 				<div
 					aria-hidden
-					className="pointer-events-none absolute bottom-0 right-1/2 z-10 aspect-square w-[50%] translate-x-1/2 translate-y-[66%] rounded-full bg-brand-green opacity-20 blur-[70px]"
+					className="pointer-events-none absolute bottom-0 right-1/2 z-10 aspect-square w-[50%] translate-x-1/2 translate-y-[66%] rounded-full bg-brand-green opacity-20 blur-[70px] will-change-transform"
 				/>
 			</div>
 			<div
