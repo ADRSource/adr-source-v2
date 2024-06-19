41b90711da73aea50bbc0b212d391182f9c10ce7
Hunter Jennings <jenningsdhunter@gmail.com>
2024-06-19 16:44:06 -0400

    add(draft-mode): add check to enforce `slug` parameter

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -4,10 +4,14 @@ import { redirect } from 'next/navigation';
 export function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const secret = searchParams.get('secret');
-	const slug = searchParams.get('slug') ?? '';
+	const slug = searchParams.get('slug');
 
 	if (secret !== process.env.DRAFT_SECRET) {
 		return new Response('Invalid token', { status: 401 });
+	}
+
+	if (slug == null) {
+		return new Response('Missing slug', { status: 400 });
 	}
 
 	draftMode().enable();
