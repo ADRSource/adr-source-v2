6e8e1e60c0c555d3d54b0846da3ddba2efca6d6b
Hunter Jennings <jenningsdhunter@gmail.com>
2024-06-30 15:05:55 -0400

    feat(deps): adding throttle fn

--- src/api/about.ts
+++ src/api/about.ts
@@ -1,12 +1,11 @@
 import { unstable_cache } from 'next/cache';
 import { cmsRequest } from '~/graphql/cms';
+import { throttle } from '~/utils/throttle';
 
-export const getAboutPage = unstable_cache(
-	(preview: boolean) => {
-		return cmsRequest(preview).GetAboutPage();
-	},
-	['about'],
-	{
-		tags: ['about'],
-	},
-);
+const throttledGetAboutPage = throttle((preview: boolean) => {
+	return cmsRequest(preview).GetAboutPage();
+});
+
+export const getAboutPage = unstable_cache(throttledGetAboutPage, ['about'], {
+	tags: ['about'],
+});
