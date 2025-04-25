1a58250f31bd4593d1b2aa033b33b784d3385f6d
Hunter Jennings <jenningsdhunter@gmail.com>
2025-04-24 22:17:40 -0400

    feat: Fetch and display case managers in BlockTeam
    - Fetches case managers data from `getCaseManagersList`
    - Renders case managers below neutrals in the BlockTeam component
    - Displays case managers' names and headshots using `MemberListItem`

--- src/components/blocks/block-team/block-team.tsx
+++ src/components/blocks/block-team/block-team.tsx
@@ -1,5 +1,5 @@
 import { draftMode } from 'next/headers';
-import { getNeutralsList } from '~/api/member';
+import { getCaseManagersList, getNeutralsList } from '~/api/member';
 import { Container } from '~/components/container';
 import { extractMemberFromNeutral } from '~/components/member-list-item/extract-member-neutral';
 import { MemberListItem } from '~/components/member-list-item/member-list-item';
@@ -7,8 +7,12 @@ import { heading } from '~/components/ui/text';
 
 export async function BlockTeam() {
   const preview = (await draftMode()).isEnabled;
-  const result = await getNeutralsList(preview);
-  const { neutralList } = result;
+  const [neutralsResult, caseManagersResult] = await Promise.all([
+    getNeutralsList(preview),
+    getCaseManagersList(preview),
+  ]);
+  const { neutralList } = neutralsResult;
+  const { caseManagerList } = caseManagersResult;
 
   return (
     <Container>
@@ -21,16 +25,38 @@ export async function BlockTeam() {
         >
           Team
         </h2>
-        <div>
-          <ul className="relative mx-auto max-w-[1058px]">
-            {neutralList?.neutrals.map((neutral) => {
-              const member = extractMemberFromNeutral(neutral);
+        <div className="mx-auto w-full max-w-[1058px] stack-y-6">
+          <div className="stack-y-4">
+            <h3 className={heading({ type: '6' })}>Neutrals</h3>
+            <ul className="relative">
+              {neutralList?.neutrals.map((neutral) => {
+                const member = extractMemberFromNeutral(neutral);
 
-              if (!member) return null;
+                if (!member) return null;
 
-              return <MemberListItem key={neutral.id} member={member} />;
-            })}
-          </ul>
+                return <MemberListItem key={neutral.id} member={member} />;
+              })}
+            </ul>
+          </div>
+          <div className="stack-y-4">
+            <h3 className={heading({ type: '6' })}>Case Managers</h3>
+            <ul className="relative">
+              {caseManagerList?.caseManagers.map((caseManager) => {
+                const { memberPage } = caseManager;
+                const { slug } = memberPage ?? {};
+
+                if (!memberPage) return null;
+
+                const m = {
+                  url: slug ?? '',
+                  name: caseManager.info.name,
+                  headshot: caseManager.info.headshot.url,
+                };
+
+                return <MemberListItem hasSchedule={false} key={caseManager.id} member={m} />;
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </Container>
